3d5232d03fbe94fd09801fb858c0398941269baf
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-21 08:57:32 +0600

    interpolation and multiline string

--- chapter 3 (string)/main.js
+++ chapter 3 (string)/main.js
@@ -1,1 +1,8 @@
-console.log('super simple dev')
+var name = 'Shihab';
+console.log(name);
+// template string ${}-> interpolation
+console.log(`Items (${1 + 1}) : $${(2095 + 799) / 100}`);
+var str = `MY name is 
+SAJJATUL ISLAM
+`;
+console.log(str);
