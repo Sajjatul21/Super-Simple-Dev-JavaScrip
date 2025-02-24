d3f66b072aaddf6cf28690754dc17b611e563dc5
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-24 09:55:27 +0600

    add the function

--- 11 Arrays and Loops/script/2-todo-list.js
+++ 11 Arrays and Loops/script/2-todo-list.js
@@ -1,19 +1,21 @@
 const todoList = ['make dinner', 'wash dishes'];
-let todoListHTML = '';
-for (let i = 0; i < todoList.length; i++) {
-    const todo = todoList[i];
-    const html = `<p>${todo}</p>`;
-    todoListHTML += html;
+renderTodoList();
+function renderTodoList() {
+    let todoListHTML = '';
+    for (let i = 0; i < todoList.length; i++) {
+        const todo = todoList[i];
+        const html = `<p>${todo}</p>`; // called generating the HTML
+        todoListHTML += html;
+    }
+    document.querySelector('.js-todo-list').innerHTML = todoListHTML;
 }
-/* console.log(todoList);
-console.log(todoListHTML); */
-document.querySelector('.js-todo-list').innerHTML = todoListHTML;
 
 function addTodo() {
     const inputElement = document.querySelector('.js-name-input');
     const name = inputElement.value;
     todoList.push(name);
-    console.log(todoList);
+    // console.log(todoList);
     inputElement.value = '';
+    renderTodoList();
 }
 
