a1d208e3f4b56f26653bf02a0fda2db1b1a3ae20
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-24 10:40:00 +0600

    store nname and date in todoList array and display it

--- 11 (Part 2) Arrays and Loops/script/2-todo-list.js
+++ 11 (Part 2) Arrays and Loops/script/2-todo-list.js
@@ -33,7 +33,14 @@ function renderTodoList() {
 function addTodo() {
     const inputElement = document.querySelector('.js-name-input');
     const name = inputElement.value;
-    todoList.push(name);
+    const dateInputElement = document.querySelector('.js-due-date-input');
+    const dueDate = dateInputElement.value;
+    todoList.push({
+        /*   name: name,
+          dueDate: dueDate */
+        name,
+        dueDate
+    });
     inputElement.value = '';
     renderTodoList();
 }
