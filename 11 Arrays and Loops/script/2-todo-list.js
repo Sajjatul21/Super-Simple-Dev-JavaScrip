b0e41ab610617aaefdb75303c8a7fd036c36b574
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-24 09:49:12 +0600

    show to default valeu make dinner and wash dishes . but when we click add button new item not added

--- 11 Arrays and Loops/script/2-todo-list.js
+++ 11 Arrays and Loops/script/2-todo-list.js
@@ -1,4 +1,14 @@
-const todoList = [];
+const todoList = ['make dinner', 'wash dishes'];
+let todoListHTML = '';
+for (let i = 0; i < todoList.length; i++) {
+    const todo = todoList[i];
+    const html = `<p>${todo}</p>`;
+    todoListHTML += html;
+}
+/* console.log(todoList);
+console.log(todoListHTML); */
+document.querySelector('.js-todo-list').innerHTML = todoListHTML;
+
 function addTodo() {
     const inputElement = document.querySelector('.js-name-input');
     const name = inputElement.value;
