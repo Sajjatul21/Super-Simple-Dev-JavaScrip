a105cf6efebbf9ae8a5db9fab1f204026305b7ee
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-24 20:26:00 +0600

    add eventlistiner in rock paper scissors

--- 12 Advanced Functions (Part 2)/script/rock-paper-scissor.js
+++ 12 Advanced Functions (Part 2)/script/rock-paper-scissor.js
@@ -9,7 +9,7 @@ let isAutoPlaying = false;
 let intervalId;
 function autoPlay() {
     if (!isAutoPlaying) {
-        intervalId = setInterval(()=> {
+        intervalId = setInterval(() => {
             const playerMove = pickComputerMove();
             playGame(playerMove);
         }, 1000);
@@ -20,6 +20,18 @@ function autoPlay() {
         isAutoPlaying = false;
     };
 }
+
+document.querySelector('.js-rock-button').addEventListener('click', () => {
+    playGame('rock');
+});
+
+document.querySelector('.js-paper-button').addEventListener('click', () => {
+    playGame('paper');
+});
+document.querySelector('.js-scissors-button').addEventListener('click', () => {
+    playGame('scissors');
+});
+
 function playGame(playerMove) {
     const computerMove = pickComputerMove();
     let result = '';
