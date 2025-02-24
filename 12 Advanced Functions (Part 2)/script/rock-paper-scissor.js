f227b22790918f45493661452afc9fbded2251d9
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-24 20:34:39 +0600

    keybord event

--- 12 Advanced Functions (Part 2)/script/rock-paper-scissor.js
+++ 12 Advanced Functions (Part 2)/script/rock-paper-scissor.js
@@ -32,6 +32,20 @@ document.querySelector('.js-scissors-button').addEventListener('click', () => {
     playGame('scissors');
 });
 
+document.body.addEventListener('keydown', (event) => {
+    // console.log('keydown')
+    // console.log(event.key)
+    if (event.key === 'r') {
+        playGame('rock');
+    }
+    else if (event.key === 'p') {
+        playGame('paper');
+    }
+    else if (event.key === 's') {
+        playGame('scissors');
+    }
+});
+
 function playGame(playerMove) {
     const computerMove = pickComputerMove();
     let result = '';
