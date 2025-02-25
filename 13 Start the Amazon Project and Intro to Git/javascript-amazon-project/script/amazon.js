428e02fade7d5adb927732f8327ff84bbef77355
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-25 11:38:26 +0600

    add cart quantity

--- 13 Start the Amazon Project and Intro to Git/javascript-amazon-project/script/amazon.js
+++ 13 Start the Amazon Project and Intro to Git/javascript-amazon-project/script/amazon.js
@@ -74,8 +74,13 @@ document.querySelectorAll('.js-add-to-cart')
           quantity: 1
         });
       }
+      let cartQuantity = 0;
+      cart.forEach(item => {
+        cartQuantity += item.quantity;
+      });
+      /* console.log(cartQuantity);
+      console.log(cart); */
+      document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
 
-
-      console.log(cart);
     });
   });
