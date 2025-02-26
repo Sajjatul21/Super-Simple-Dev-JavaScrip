9251faf99f7c9521eb2a6ccbdf7527025733e271
Sajjatul21 <sajjatulislam565@gmail.com>
2025-02-26 14:26:12 +0600

    save cart in local storage

--- 13 Start the Amazon Project and Intro to Git/javascript-amazon-project/script/amazon.js
+++ 13 Start the Amazon Project and Intro to Git/javascript-amazon-project/script/amazon.js
@@ -1,7 +1,7 @@
 import { cart, addToCart } from '../script/card.js';
 import { products } from '../data/products.js';
 import { formateCurrency } from './utils/money.js';
-console.log(products);
+
 let productsHtml = ''; // accumulator variable
 products.forEach(product => {
   // accumulator pattern
--- 13 Start the Amazon Project and Intro to Git/javascript-amazon-project/script/checkout.js
+++ 13 Start the Amazon Project and Intro to Git/javascript-amazon-project/script/checkout.js
@@ -93,7 +93,6 @@ document.querySelectorAll('.js-delete-link').forEach(link => {
         const productId = link.dataset.productId;
         removeFromCart(productId);
         const container = document.querySelector(`.js-cart-item-container-${productId}`);
-        // console.log(container);
         container.remove();
 
 
