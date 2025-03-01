51f86f502f9353cce26c8b032df6f74d1104454c
Sajjatul21 <sajjatulislam565@gmail.com>
2025-03-01 19:46:41 +0600

    basic test and edge cases

--- 16 Testing (javascript-amazon-project)/tests/moneyTest.js
+++ 16 Testing (javascript-amazon-project)/tests/moneyTest.js
@@ -1,9 +1,32 @@
 import { formateCurrency } from "../script/utils/money.js";
 
-
+// 1. basic test cases
 if (formateCurrency(2095) === '20.95') {
     console.log('passed');
 }
 else {
     console.log('failed');
 }
+// 2. edge cases
+if (formateCurrency(0) === '0.00') {
+    console.log('passed');
+}
+else {
+    console.log('failed');
+}
+
+if (formateCurrency(2000.5) === '20.01') {
+    console.log('passed');
+}
+else {
+    console.log('failed');
+}
+
+/* 
+    2 type of test cases
+    1. basic test cases
+        = tests if the code is working
+    2. edge cases
+        = test with values that are tricky
+
+*/
