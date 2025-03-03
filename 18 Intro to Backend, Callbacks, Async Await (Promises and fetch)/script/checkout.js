51ed3c0ff0fa556fca412dfc08d3d24e3f53a975
Sajjatul21 <sajjatulislam565@gmail.com>
2025-03-03 14:15:50 +0600

    access value

--- 18 Intro to Backend, Callbacks, Async Await (Promises and fetch)/script/checkout.js
+++ 18 Intro to Backend, Callbacks, Async Await (Promises and fetch)/script/checkout.js
@@ -3,6 +3,24 @@ import { renderPaymentSummary } from './checkout/paymentSummary.js';
 import { loadProducts } from '../data/products.js';
 import { loadCart } from './card.js';
 
+Promise.all([
+    new Promise((resolve) => {
+        loadProducts(() => {
+            resolve('value-1');
+        });
+    }),
+    new Promise(resolve => {
+        loadCart(() => {
+            resolve();
+        });
+    })
+
+]).then((value) => {
+    console.log(value);
+    renderOrderSummary();
+    renderPaymentSummary();
+});
+/* 
 new Promise((resolve) => {
     loadProducts(() => {
         resolve('value-1');
@@ -18,7 +36,7 @@ new Promise((resolve) => {
     renderOrderSummary();
     renderPaymentSummary();
 })
-
+ */
 /* 
 loadProducts(() => {
     loadCart(() => {
