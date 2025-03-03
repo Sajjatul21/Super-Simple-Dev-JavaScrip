0a22fd73759f9ab2c12beb1f820cadab941d2008
Sajjatul21 <sajjatulislam565@gmail.com>
2025-03-03 12:49:32 +0600

    promise

--- 18 Intro to Backend, Callbacks, Async Await (Promises and fetch)/script/checkout.js
+++ 18 Intro to Backend, Callbacks, Async Await (Promises and fetch)/script/checkout.js
@@ -2,6 +2,10 @@ import { renderOrderSummary } from './checkout/orderSummary.js';
 import { renderPaymentSummary } from './checkout/paymentSummary.js';
 import { loadProducts } from '../data/products.js';
 
+new Promise(() => {
+    console.log('Promise'); //when create a promise  it run the inner function immediately
+});
+
 loadProducts(() => {
     renderOrderSummary();
     renderPaymentSummary();
