db5489e53ad4147a05b21c397d617387005ad6e2
Sajjatul21 <sajjatulislam565@gmail.com>
2025-03-03 09:21:20 +0600

    default class

--- Inheritance 17.2/data/products.js
+++ Inheritance 17.2/data/products.js
@@ -71,6 +71,10 @@ const tShirt = new Clothing(
 console.log(tShirt);
 console.log(tShirt.getPrice());
 
+const date = new Date();
+console.log(date);
+console.log(date.toLocaleDateString());
+
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
