3286310053755c10bf9d0bc79742a4c8f592c252
Sajjatul21 <sajjatulislam565@gmail.com>
2025-03-03 10:40:07 +0600

    create a http request and send in supersimpledev backend

---
+++ 18 Intro to Backend, Callbacks, Async Await/data/backend-practice.js
@@ -0,0 +1,13 @@
+const xhr = new XMLHttpRequest(); // create a new  HTTP message to sand to the backend;
+// open() first parameter : what type of message we can send;
+// GET : get some information from the backend
+// open() second parameter :second parameter tells a computer where to send this to send this HTTP massage;
+// URL = Uniform Resource Locator
+
+/* 
+    https:// amazon.com
+    https: to communicate with this computer after ( s)means we're using a secure version of http
+     amazon.com: this is like an address it points to another computer on the internet. amazon.com points to one of amazon's backend computers 
+*/
+xhr.open('GET', 'https://supersimplebackend.dev');
+xhr.send(); // send a request on supersimplebackend  backend;
--- 18 Intro to Backend, Callbacks, Async Await/script/checkout.js
+++ 18 Intro to Backend, Callbacks, Async Await/script/checkout.js
@@ -1,6 +1,6 @@
 import { renderOrderSummary } from './checkout/orderSummary.js';
 import { renderPaymentSummary } from './checkout/paymentSummary.js';
-// import '../script/cart-class.js';
+import '../data/backend-practice.js'
 
 renderOrderSummary();
 renderPaymentSummary();
