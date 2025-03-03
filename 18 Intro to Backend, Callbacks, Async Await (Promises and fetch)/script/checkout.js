06cf2f45bce1e2d1a743915ce1193a1a5cfc1f25
Sajjatul21 <sajjatulislam565@gmail.com>
2025-03-03 13:07:52 +0600

    when the fininsh loading then we run rendorOrdersummary and renderPaymentSummary function

--- 18 Intro to Backend, Callbacks, Async Await (Promises and fetch)/script/checkout.js
+++ 18 Intro to Backend, Callbacks, Async Await (Promises and fetch)/script/checkout.js
@@ -3,14 +3,12 @@ import { renderPaymentSummary } from './checkout/paymentSummary.js';
 import { loadProducts } from '../data/products.js';
 
 new Promise((resolve) => {
-    // resolve -lets us control when to go to the next step
-    console.log('start Promise');
-    loadProducts(() => { // first we wun this asynchronous code .then we wait for it to finish.then we run resolve.
-        console.log('finished loading');
-        resolve(); // resolve makes it go to the next step (then(()={}))
+    loadProducts(() => {
+        resolve();
     });
 }).then(() => {
-    console.log('next step');
+    renderOrderSummary();
+    renderPaymentSummary();
 });
 
 /* 
