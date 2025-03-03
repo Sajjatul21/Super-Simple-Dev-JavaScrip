3d7bfc1573cee2ea4a620f3776fba37a3ba46d15
Sajjatul21 <sajjatulislam565@gmail.com>
2025-03-03 14:08:10 +0600

    value

--- 18 Intro to Backend, Callbacks, Async Await (Promises and fetch)/script/checkout.js
+++ 18 Intro to Backend, Callbacks, Async Await (Promises and fetch)/script/checkout.js
@@ -4,17 +4,18 @@ import { loadProducts } from '../data/products.js';
 import { loadCart } from './card.js';
 
 new Promise((resolve) => {
-    loadProducts(() => { //1. wait for it to finish
-        resolve();       //2. go to the next step
+    loadProducts(() => {
+        resolve('value-1');
     });
-}).then(() => {
+}).then((value) => {
+    console.log(value);
     new Promise(resolve => {
-        loadCart(() => {   //3. wait for it to finish
-            resolve();     //4. go to the next step
+        loadCart(() => {
+            resolve();
         });
     });
-}).then(() => {             //5. run this code 
-    renderOrderSummary(); 
+}).then(() => {
+    renderOrderSummary();
     renderPaymentSummary();
 })
 
