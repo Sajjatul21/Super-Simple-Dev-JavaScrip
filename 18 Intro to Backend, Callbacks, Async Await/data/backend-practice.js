4bf21119a6ecc1199ca1fd20f687e90b40f10e6b
Sajjatul21 <sajjatulislam565@gmail.com>
2025-03-03 11:28:19 +0600

    give us different type of data in response

--- 18 Intro to Backend, Callbacks, Async Await/data/backend-practice.js
+++ 18 Intro to Backend, Callbacks, Async Await/data/backend-practice.js
@@ -3,18 +3,9 @@ const xhr = new XMLHttpRequest(); // create a new  HTTP message to sand to the b
 xhr.addEventListener('load', () => {
     console.log(xhr.response);
 });
-// xhr.open('GET', 'https://supersimplebackend.dev/hello');
-xhr.open('GET', 'https://supersimplebackend.dev/not-supported');
+// xhr.open('GET', 'https://supersimplebackend.dev/hello');// response type date text
+// xhr.open('GET', 'https://supersimplebackend.dev/products/first'); // response type date Json
+// xhr.open('GET', 'https://supersimplebackend.dev/documentation'); // response type date HTML
+xhr.open('GET', 'https://supersimplebackend.dev/images/apple.jpg'); // response type date images
 xhr.send();
-
-// a backend only supports a certain set of url paths
-// whenever we get a response from the backend.  the backend also gives us a status code
-/* 
-    status code 
-    starts with 4 or 5 (400, 405, 5000) = failed
-    starting with 4 means it was our problem we sent the request
-    tarting with five means it was the backends problem for example the backend crashed
-    
-    Starts with 2 (200, 201, 204 = succeeded)
-*/
 
