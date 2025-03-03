a7792f64d1e0b504cf8c2c9dc535b63354c2170d
Sajjatul21 <sajjatulislam565@gmail.com>
2025-03-03 11:15:00 +0600

    error

--- 18 Intro to Backend, Callbacks, Async Await/data/backend-practice.js
+++ 18 Intro to Backend, Callbacks, Async Await/data/backend-practice.js
@@ -4,5 +4,17 @@ xhr.addEventListener('load', () => {
     console.log(xhr.response);
 });
 // xhr.open('GET', 'https://supersimplebackend.dev/hello');
-xhr.open('GET', 'https://supersimplebackend.dev/products/first');
+xhr.open('GET', 'https://supersimplebackend.dev/not-supported');
 xhr.send();
+
+// a backend only supports a certain set of url paths
+// whenever we get a response from the backend.  the backend also gives us a status code
+/* 
+    status code 
+    starts with 4 or 5 (400, 405, 5000) = failed
+    starting with 4 means it was our problem we sent the request
+    tarting with five means it was the backends problem for example the backend crashed
+    
+    Starts with 2 (200, 201, 204 = succeeded)
+*/
+
